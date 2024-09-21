8bd262d399231d403fea19251e8531e6ef0bbcb4
mattgo327 <matiasgaray104@gmail.com>
2024-09-21 09:51:35 -0400

    Testing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from CircleCI and Google Cloud!");
+  res.send("Hello from CircleCI and Google Cloud! Testing now!");
 });
 
 app.listen(PORT, () => {
