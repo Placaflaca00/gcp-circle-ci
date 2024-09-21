93be2a7e4e0fba0eaa28cf7adc9f75c83bdb909c
mattgo327 <matiasgaray104@gmail.com>
2024-09-21 09:47:39 -0400

    Fix console syntax

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from CircleCI and Google Cloud!');
+app.get("/", (req, res) => {
+  res.send("Hello from CircleCI and Google Cloud!");
 });
 
 app.listen(PORT, () => {
